0c3d5dc89a748514da776efdfa8fe0ec701d7ec2
Niklas von Moers <moers@meteolytix.de>
2020-01-04 21:09:47 +0100

    init: generic board functions

---
+++ boardFunctions.R
@@ -0,0 +1,15 @@
+isSubset <- function(v1, v2) {
+  all(v1 %in% v2)
+}
+
+# Does vec contain length consecutive numbers?
+hasSeq <- function(vec, length) {
+  if (length > length(vec)) return (F)
+  for (i in 1:(length(vec) - (length - 1))) {
+    subVec <- vec[i:(i + (length - 1))]
+    if (all(subVec == subVec[1]:(subVec[1] + length - 1))) return(T)
+  }
+  return (F)
+}
+
+rotate <- function(x) t(apply(x, 2, rev))
