d49bc205482fe1983a551afc708ec8ccf1cca007
Niklas von Moers <moers@meteolytix.de>
2020-01-06 09:46:55 +0100

    chore: test game human vs ai

--- testGame.R
+++ testGame.R
@@ -5,10 +5,10 @@ ais <- list()
 ais[[1]] <- ai1
 ais[[2]] <- ai2
 game <- ConnectN$new(n = 4L, ncol = 7L, nrow = 6L, verbose = F)
-playGame(players = c(1, 2), ais = ais, maxIts = 1000, game = game)
+playGame(players = c(1, 2), ais = ais, maxIts = 10000, game = game)
 ai2$oddsBestMove <- 1
 game$clearBoard()
 ais <- list()
 ais[[2]] <- ai2
 game$verbose <- T
-playGame(players = c(1, 2), humanPlayers = integer(), ais = ais, game = game)
+playGame(players = c(1, 2), humanPlayers = 1, ais = ais, game = game)
